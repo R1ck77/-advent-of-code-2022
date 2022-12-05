fc8edfdc59422596847158f04b4f37ed01279927
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2022-12-05 14:05:21 +0100

    Add boilerplate code for day 6

---
+++ day06.el
@@ -0,0 +1,10 @@
+(require 'dash)
+(require 'advent-utils)
+
+(defun day06/part-1 (lines)
+  (error "Not yet implemented"))
+
+(defun day06/part-2 (lines)
+  (error "Not yet implemented"))
+
+(provide 'day06)
