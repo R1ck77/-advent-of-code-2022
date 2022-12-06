f35dfbb80bf10235fa149c7ebdf58bc1b70e9726
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2022-12-06 08:03:21 +0100

    Day 6 part 2 solved

--- day06-test.el
+++ day06-test.el
@@ -1,7 +1,7 @@
 (require 'day06)
 (require 'buttercup)
 
-(describe "Day 6"
+(describe "--- Day 6: Tuning Trouble ---"
   (describe "part 1"
     (it "replicates the example"
       (expect (day06/part-1 (advent/read-problem-lines 6 :example))
@@ -9,10 +9,10 @@
     (it "solves the problem"
       (expect (day06/part-1 (advent/read-problem-lines 6 :problem))
               :to-equal '(1816))))
-  (xdescribe "part 2"
+  (describe "part 2"
     (it "replicates the example"
       (expect (day06/part-2 (advent/read-problem-lines 6 :example))
-              :to-equal 42))
-    (xit "solves the problem"
+              :to-equal '(19 23 23 29 26)))
+    (it "solves the problem"
       (expect (day06/part-2 (advent/read-problem-lines 6 :problem))
-              :to-equal 42))))
+              :to-equal '(2625)))))
--- day06.el
+++ day06.el
@@ -7,14 +7,24 @@
 (defun day06/all-different? (group)
   (= (length group) (hash-table-count (advent/set-from group))))
 
-(defun day06/get-packet-start-index (stream)
-  (+ 4 (-find-index #'day06/all-different?
-                    (-partition-in-steps 4 1 (split-string stream "" t)))))
+(defun day06/get-packet-start-index (stream size)
+  (+ size (-find-index #'day06/all-different?
+                    (-partition-in-steps size 1 (split-string stream "" t)))))
+
+(defun day06/get-packet-start-index (stream size)
+  (+ size (-find-index #'day06/all-different?
+                       (-partition-in-steps size 1 (split-string stream "" t)))))
+
+(defun day06/get-start-packet-index (stream)
+  (day06/get-packet-start-index stream 4))
 
 (defun day06/part-1 (streams)
-  (-map #'day06/get-packet-start-index streams))
+  (-map #'day06/get-start-packet-index streams))
 
-(defun day06/part-2 (lines)
-  (error "Not yet implemented"))
+(defun day06/get-start-message-index (stream)
+  (day06/get-packet-start-index stream 14))
+
+(defun day06/part-2 (streams)
+  (-map #'day06/get-start-message-index streams))
 
 (provide 'day06)
