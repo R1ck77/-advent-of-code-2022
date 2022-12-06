e5fdb87e0d88e8257a3530a28ebdb50a518c6f12
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2022-12-06 07:58:52 +0100

    Day 6 part 1 solved

--- day06.el
+++ day06.el
@@ -1,8 +1,18 @@
 (require 'dash)
 (require 'advent-utils)
 
-(defun day06/part-1 (lines)
-  (error "Not yet implemented"))
+(defvar example (advent/read-problem-lines 6 :example))
+(defvar problem (advent/read-problem-lines 6 :problem))
+
+(defun day06/all-different? (group)
+  (= (length group) (hash-table-count (advent/set-from group))))
+
+(defun day06/get-packet-start-index (stream)
+  (+ 4 (-find-index #'day06/all-different?
+                    (-partition-in-steps 4 1 (split-string stream "" t)))))
+
+(defun day06/part-1 (streams)
+  (-map #'day06/get-packet-start-index streams))
 
 (defun day06/part-2 (lines)
   (error "Not yet implemented"))
