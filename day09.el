437a4d4ae18f1d3493ade0876949ecedb38ac643
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2022-12-09 09:57:28 +0100

    Add the data for day 9

--- day09.el
+++ day09.el
@@ -1,6 +1,22 @@
 (require 'dash)
 (require 'advent-utils)
 
+(defvar example (advent/read-problem-lines 9 :example))
+(defvar problem (advent/read-problem-lines 9 :problem))
+
+(defconst day09/string-to-direction '(("R" . :right)
+                                      ("L" . :left)
+                                      ("U" . :up)
+                                      ("D" . :down)))
+
+(defun day09/read-line (line)
+  (seq-let (string-dir count) (split-string line)
+    (-repeat (string-to-number count)
+             (cdr (assoc string-dir day09/string-to-direction)))))
+
+(defun day09/read-problem (lines)
+  (-mapcat #'day09/read-line lines))
+
 (defun day09/part-1 (lines)
   (error "Not yet implemented"))
 
