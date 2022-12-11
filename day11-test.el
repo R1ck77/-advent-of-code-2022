55ba7d812a539f09f4e610b80d4710b633b2e3f0
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2022-12-11 17:50:38 +0100

    Refactor part 1 to make it suitable for part 2

--- day11-test.el
+++ day11-test.el
@@ -1,18 +1,18 @@
 (require 'day11)
 (require 'buttercup)
 
-(describe "Day 11"
+(describe "--- Day 11: Monkey in the Middle ---"
   (describe "part 1"
     (it "replicates the example"
-      (expect (day11/part-1 (advent/read-problem-text 11 :example))
+      (expect (day11/part-1 (advent/read-blocks-of-lines 11 :example))
               :to-be 10605 ))
     (it "solves the problem"
-      (expect (day11/part-1 (advent/read-problem-text 11 :problem))
+      (expect (day11/part-1 (advent/read-blocks-of-lines 11 :problem))
               :to-be 99840)))
   (xdescribe "part 2"
     (it "replicates the example"
-      (expect (day11/part-2 (advent/read-problem-text 11 :example))
+      (expect (day11/part-2 (advent/read-blocks-of-lines 11 :example))
               :to-be 2713310158))
     (xit "solves the problem"
-      (expect (day11/part-2 (advent/read-problem-text 11 :problem))
+      (expect (day11/part-2 (advent/read-blocks-of-lines 11 :problem))
               :to-be 42))))
--- day11.el
+++ day11.el
@@ -5,33 +5,89 @@
 (setq example (advent/read-blocks-of-lines 11 :example))
 (setq problem (advent/read-blocks-of-lines 11 :problem))
 
-(setq e (eval (read example)))
-(setq p (eval (read problem)))
+(defun day11/read-last-int (string)
+  (string-to-number
+   (car
+    (last
+     (split-string string)))))
 
-(defun day11/extract-monkeys (monkey-items post-process-f)
-  (let ((operation (plist-get monkey-items :operation))
-        (test (plist-get monkey-items :test))
-        (f post-process-f))
-   (lambda (old)
-     (let ((new (funcall f (funcall operation old))))
-       (list new (funcall test new))))))
+(defun day11/read-false-destination (false-str)
+  (day11/read-last-int false-str))
 
-(defun day11/extract-items (monkey-items)
-  (--map (list :fact (list it)
-               :add 0)
-         (plist-get monkey-items :items)))
+(defun day11/read-true-destination (true-str)
+  (day11/read-last-int true-str))
 
-(defun day11/separate-data-from-functions (monkeys-items post-process-f)
-  (list (--map (day11/extract-monkeys it post-process-f) monkeys-items)
-        (-map #'day11/extract-items monkeys-items)))
+(defun day11/read-test (test-str)
+  (day11/read-last-int test-str))
 
-(defun day11/replace-element (a-list index new-value)
-  (let ((copy (apply #'list a-list)))
-    (setcar (nthcdr index copy) new-value)
-    copy))
+(defun day11/read-operation (operation-str)
+  (let ((tokens (nthcdr 3 (split-string operation-str "[= ]+"))))
+    (seq-let (op1 operator op2) tokens
+      (advent/assert (or (not (string= op1 op2)) (string= operator "*")) "old + old not supported (or expected…)")
+      (if (and (string= op1 op2))
+          (list :square nil)
+        (if (string= operator "+")
+            (list :+ (string-to-number op2))
+          (list :* (string-to-number op2)))))))
 
-(defun day11/move-item (monkey index all-items &optional inspections-f)
-  (funcall (or inspections-f #'identity) index)
+(defun day11/read-items (items-str)
+  (-map #'string-to-number (rest (split-string items-str "[:,]"))))
+
+(defun day11/read-monkey (monkey-str)
+  (string-to-number (cadr (split-string monkey-str))))
+
+(defun day11/read-block (blk)
+  (seq-let (monkey-str items-str operation-str test-str true-str false-str) blk
+    (list :monkey (day11/read-monkey monkey-str)
+          :items (day11/read-items items-str)
+          :operation (day11/read-operation operation-str)
+          :test (day11/read-test test-str)
+          :true (day11/read-true-destination true-str)
+          :false (day11/read-false-destination false-str))))
+
+(defun day11/read-data (blocks)
+  (-map #'day11/read-block blocks))
+
+(defun day11/create-monkey-functions (monkey preprocessed-data monkey-function-generator-f)
+  (funcall monkey-function-generator-f preprocessed-data monkey))
+
+(defun day11/create-item-list (monkey preprocessed-data item-to-object-f)
+  (--map (funcall item-to-object-f preprocessed-data it)
+         (plist-get monkey :items)))
+
+(defun day11/prepare-data (monkeys preprocess-f item-to-object-f monkey-function-generator-f)
+  (let ((preprocessed-data (funcall preprocess-f monkeys)))
+    (list (--map (day11/create-item-list it preprocessed-data item-to-object-f)
+                 monkeys)
+          (--map (day11/create-monkey-functions it preprocessed-data monkey-function-generator-f)
+                 monkeys))))
+
+(defun day11/can-be-divided (a b)
+  (zerop (mod a b)))
+
+(defun day11/no-stress-monkey-function-generator (_ monkey counter-bump-f)
+  ;; TODO/FIXME can be easily optimized (remove the cond/case/if and plist-get!)
+  (lambda (item)
+    (funcall counter-bump-f (plist-get monkey :monkey))
+    (let ((operation (plist-get monkey :operation)))
+     (let ((result (/ (case (car operation)
+                        (:square (* item item))
+                        (:+ (+ item (cadr operation)))
+                        (:* (* item (cadr operation)))
+                        (t (error (format "Unexpected operation (%s)" operation))))
+                      3)))
+    (if (day11/can-be-divided result (plist-get monkey :test))
+        (list result (plist-get monkey :true))
+      (list result (plist-get monkey :false)))       
+       ))))
+
+(defun day11/prepare-no-stress-data (monkeys counter-update-f)
+  (day11/prepare-data monkeys #'not
+                      (lambda (_ item) item)
+                      (lambda (_ monkey)
+                        (day11/no-stress-monkey-function-generator nil monkey counter-update-f))))
+
+(defun day11/move-item (monkey index all-items)
   (let* ((items (elt all-items index))
         (value-position (funcall monkey (car items))))
     (seq-let (value position) value-position      
@@ -39,49 +95,44 @@
                            position
                            (append (elt all-items position) (list value))))))
 
-(defun day11/update-items (monkey index all-items &optional inspection-f)
+(defun day11/update-items (monkey index all-items)
   (let ((monkey-items (elt all-items index)))
     (if (not monkey-items)
       all-items
       (day11/update-items monkey
                           index
-                          (day11/move-item monkey index all-items inspection-f)
-                          inspection-f))))
+                          (day11/move-item monkey index all-items)))))
 
 (defun day11/bump-counter (counter index)
   (aset counter index (1+ (aref counter index))))
 
 ;;; TODO/FIXME modifies counter but the name lies!
-(defun day11/do-round (monkeys items &optional counter)
-  (let ((counter (or counter (make-vector (length monkeys) 0))))
-    (list monkeys
-          (-reduce-from (lambda (items monkey&index)
-                    (let ((index (cdr monkey&index)))
-                      (day11/update-items (car monkey&index)
-                                          index
-                                          items
-                                          (lambda (index) (day11/bump-counter counter index)))))
-                  items
-                  (--map-indexed (cons it it-index) monkeys))
-          counter)))
+(defun day11/do-round (items monkeys)
+  (list monkeys
+        (-reduce-from (lambda (items monkey&index)
+                        (day11/update-items (car monkey&index)
+                                            (cdr monkey&index)
+                                            items))
+                      items
+                      (--map-indexed (cons it it-index) monkeys))))
 
-(defun day11/get-counter-for-rounds (monkeys items index)
-  (let ((counter))
-    (--dotimes index
-     (seq-let (_ new-items updated-counter) (day11/do-round monkeys items counter)
-       (setq items new-items)
-       (setq counter updated-counter)) ;; TODO/FIXME geez
-     )
-   counter))
+(defun day11/get-counter-for-rounds (items monkeys reps)
+  (--dotimes reps
+    (seq-let (_ new-items updated-counter) (day11/do-round items monkeys)
+      (setq items new-items))))
 
 (defun day11/compute-monkey-business (counter)
   (apply #'* (-take 2 (append (-sort #'> counter) nil))))
 
 
-(defun day11/part-1 (text)
-  (let ((monkeys-items (eval (read text))))
-    (seq-let (monkeys items) (day11/separate-data-from-functions monkeys-items (lambda (x) (/ x 3)))
-        (day11/compute-monkey-business (day11/get-counter-for-rounds monkeys items 20)))))
+(defun day11/part-1 (line-blocks)
+  (let* ((raw-data (day11/read-data line-blocks))
+         (counter (make-vector (length raw-data) 0))
+         (bump-counter-f (lambda (index) (day11/bump-counter counter index))))
+    (seq-let (items monkeys) (day11/prepare-no-stress-data raw-data bump-counter-f)
+      (day11/get-counter-for-rounds items monkeys 20))
+    (day11/compute-monkey-business counter)))
+
 
 (defun day11/part-2 (text)
     (let ((monkeys-items (eval (read text))))
