691dd25bd2f4f204fe818739301ee985e52e268e
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2022-12-12 09:26:41 +0100

    Add day 12 data

--- day12-test.el
+++ day12-test.el
@@ -1,18 +1,18 @@
 (require 'day12)
 (require 'buttercup)
 
-(describe "Day 12"
+(describe "--- Day 12: Hill Climbing Algorithm ---"
   (describe "part 1"
     (it "replicates the example"
-      (expect (day12/part-1 (advent/read-problem-lines 12 :example))
-              :to-be 42 ))
+      (expect (day12/part-1 (advent/read-grid 12 :example #'identity))
+              :to-be 31 ))
     (xit "solves the problem"
-      (expect (day12/part-1 (advent/read-problem-lines 12 :problem))
+      (expect (day12/part-1 (advent/read-grid 12 :problem #'identity))
               :to-be 42)))
   (xdescribe "part 2"
     (it "replicates the example"
-      (expect (day12/part-2 (advent/read-problem-lines 12 :example))
+      (expect (day12/part-2 (advent/read-grid 12 :example #'identity))
               :to-be 42))
     (xit "solves the problem"
-      (expect (day12/part-2 (advent/read-problem-lines 12 :problem))
+      (expect (day12/part-2 (advent/read-grid 12 :problem #'identity))
               :to-be 42))))
--- day12.el
+++ day12.el
@@ -1,6 +1,9 @@
 (require 'dash)
 (require 'advent-utils)
 
+(defconst example (advent/read-grid 12 :example #'identity))
+(defconst problem (advent/read-grid 12 :problem #'identity))
+
 (defun day12/part-1 (lines)
   (error "Not yet implemented"))
 
