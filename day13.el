ad7e7eec02177054b1b73f861ac6c8f16ddf6ff0
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2022-12-13 10:21:36 +0100

    Add parsing

--- day13.el
+++ day13.el
@@ -1,5 +1,22 @@
 (require 'dash)
 (require 'advent-utils)
+(require 's)
+
+(defvar example (advent/read-blocks-of-lines 13 :example))
+(defvar problem (advent/read-blocks-of-lines 13 :problem))
+
+(defun day13/read-packet (line)
+  (read (s-replace "," " " line)))
+
+(defun day13/read-pair (line-block)
+  (-map #'day13/read-packet line-block))
+
+(defun day13/read-packets (line-blocks)
+  (-map #'day13/read-pair line-blocks))
+
+(defun day13/compare (left right)
+  
+  )
 
 (defun day13/part-1 (lines)
   (error "Not yet implemented"))
