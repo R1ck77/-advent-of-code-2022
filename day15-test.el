3f7e4860bdfda46613ae1f295c057fc42d9a9bf1
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2022-12-15 11:45:25 +0100

    Minor change to the test
    This is the first problem I know where part of the data (the y for
    part 1) was in the description and not the example file, so I have to
    update the test.

--- day15-test.el
+++ day15-test.el
@@ -4,10 +4,10 @@
 (describe "Day 15"
   (describe "part 1"
     (it "replicates the example"
-      (expect (day15/part-1 (advent/read-problem-lines 15 :example))
+      (expect (day15/part-1 (advent/read-problem-lines 15 :example) 10)
               :to-be 26))
     (xit "solves the problem"
-      (expect (day15/part-1 (advent/read-problem-lines 15 :problem))
+      (expect (day15/part-1 (advent/read-problem-lines 15 :problem) 2000000)
               :to-be 42)))
   (xdescribe "part 2"
     (it "replicates the example"
