5f614002d2dc8e0243779b5d94683f2b2d635a8b
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2022-12-17 11:05:35 +0100

    Add day 17 data

--- day17-test.el
+++ day17-test.el
@@ -1,11 +1,11 @@
 (require 'day17)
 (require 'buttercup)
 
-(describe "Day 17"
+(describe "--- Day 17: Pyroclastic Flow ---"
   (describe "part 1"
     (it "replicates the example"
       (expect (day17/part-1 (advent/read-problem-lines 17 :example))
-              :to-be 42 ))
+              :to-be 3068))
     (xit "solves the problem"
       (expect (day17/part-1 (advent/read-problem-lines 17 :problem))
               :to-be 42)))
