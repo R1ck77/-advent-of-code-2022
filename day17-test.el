a902f067daa55d84f6d6c8e75ff0885e7659aef8
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2022-12-17 11:20:14 +0100

    Read the input

--- day17-test.el
+++ day17-test.el
@@ -4,15 +4,15 @@
 (describe "--- Day 17: Pyroclastic Flow ---"
   (describe "part 1"
     (it "replicates the example"
-      (expect (day17/part-1 (advent/read-problem-lines 17 :example))
+      (expect (day17/part-1 (advent/read-problem-text 17 :example))
               :to-be 3068))
     (xit "solves the problem"
-      (expect (day17/part-1 (advent/read-problem-lines 17 :problem))
+      (expect (day17/part-1 (advent/read-problem-text 17 :problem))
               :to-be 42)))
   (xdescribe "part 2"
     (it "replicates the example"
-      (expect (day17/part-2 (advent/read-problem-lines 17 :example))
+      (expect (day17/part-2 (advent/read-problem-text 17 :example))
               :to-be 42))
     (xit "solves the problem"
-      (expect (day17/part-2 (advent/read-problem-lines 17 :problem))
+      (expect (day17/part-2 (advent/read-problem-text 17 :problem))
               :to-be 42))))
--- day17.el
+++ day17.el
@@ -2,6 +2,12 @@
 (require 'advent-utils)
 (require 's)
 
+(defconst example (advent/read-problem-text 17 :example))
+(defconst problem (advent/read-problem-text 17 :problem))
+
+(defun day17/read-problem (line)
+  (--map (if (string= "<" it) -1 1) (split-string (string-trim line) "" t)))
+
 (defun day17/part-1 (lines)
   (error "Not yet implemented"))
 
