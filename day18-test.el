c74c09bacc16a907304b3fa2cc9f49aa48f73e67
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2022-12-18 14:10:31 +0100

    Day18 part 1 solved

--- day18-test.el
+++ day18-test.el
@@ -6,9 +6,9 @@
     (it "replicates the example"
       (expect (day18/part-1 (advent/read-problem-lines 18 :example))
               :to-be 64))
-    (xit "solves the problem"
+    (it "solves the problem"
       (expect (day18/part-1 (advent/read-problem-lines 18 :problem))
-              :to-be 42)))
+              :to-be 3364)))
   (xdescribe "part 2"
     (it "replicates the example"
       (expect (day18/part-2 (advent/read-problem-lines 18 :example))
--- day18.el
+++ day18.el
@@ -11,8 +11,34 @@
 (defun day18/read-problem (lines)
   (-map #'day18/read-line lines))
 
+(defun day18/adjacent (a b)
+  (= (apply #'+ (--map (abs (- (car it) (cdr it)) ) (-zip a b))) 1))
+
+(defun day18/adjacent-indices (list index-a index-b)
+  (day18/adjacent (elt list index-a)
+                  (elt list index-b)))
+
+(defun day18/compute-adjacency (list it-index)
+  "Assume that the adjacency for i < it-index have been already accounted for"
+  (let ((i (1+ it-index))
+        (adjacencies 0))
+    (while (and (< i (length list))
+                (< adjacencies 6))
+      (when (day18/adjacent-indices list it-index i )
+        (setq adjacencies (1+ adjacencies)))
+      (setq i (1+ i)))
+    adjacencies))
+
+(defun day18/compute-all-adjacencies (list)
+  (let ((total-adjacencies 0))
+    (--dotimes (length list)
+      (setq total-adjacencies (+ total-adjacencies (day18/compute-adjacency list it))))
+    total-adjacencies))
+
 (defun day18/part-1 (lines)
-  (error "Not yet implemented"))
+  (let ((rocks (day18/read-problem lines)))
+    (- (* 6 (length rocks))
+       (* 2 (day18/compute-all-adjacencies rocks)))))
 
 (defun day18/part-2 (lines)
   (error "Not yet implemented"))
