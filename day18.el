5d8487ee170ec4c994bc1ee570c04660653f8c83
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2022-12-18 13:57:15 +0100

    Read the problem data

--- day18.el
+++ day18.el
@@ -2,6 +2,15 @@
 (require 'advent-utils)
 (require 's)
 
+(defconst example (advent/read-problem-lines 18 :example))
+(defconst problem (advent/read-problem-lines 18 :problem))
+
+(defun day18/read-line (line)
+  (-map #'string-to-number (split-string line ",")))
+
+(defun day18/read-problem (lines)
+  (-map #'day18/read-line lines))
+
 (defun day18/part-1 (lines)
   (error "Not yet implemented"))
 
