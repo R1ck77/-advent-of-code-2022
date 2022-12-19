cd7679984451fe88390786357aa011bb65e9d755
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2022-12-19 09:11:39 +0100

    Add data for day 19

--- day19-test.el
+++ day19-test.el
@@ -1,11 +1,11 @@
 (require 'day19)
 (require 'buttercup)
 
-(describe "Day 19"
+(describe "--- Day 19: Not Enough Minerals ---"
   (describe "part 1"
     (it "replicates the example"
       (expect (day19/part-1 (advent/read-problem-lines 19 :example))
-              :to-be 42))
+              :to-be 33))
     (xit "solves the problem"
       (expect (day19/part-1 (advent/read-problem-lines 19 :problem))
               :to-be 42)))
