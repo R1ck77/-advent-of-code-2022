7c544d27d0fd0c055c744b47f8bc096b8623f4cb
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2022-12-21 10:16:04 +0100

    Add files and data for day 21

---
+++ day21.el
@@ -0,0 +1,14 @@
+(require 'dash)
+(require 'advent-utils)
+(require 's)
+
+(defconst example (advent/read-problem-lines 21 :example))
+(defconst problem (advent/read-problem-lines 21 :problem))
+
+(defun day21/part-1 (lines)
+  (error "Not yet implemented"))
+
+(defun day21/part-2 (lines)
+  (error "Not yet implemented"))
+
+(provide 'day21)
