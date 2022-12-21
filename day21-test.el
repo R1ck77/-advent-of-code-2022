58f1cc74b3115976c695051985dbf43c78664866
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2022-12-21 12:02:08 +0100

    Day 21 part 2 solved

--- day21-test.el
+++ day21-test.el
@@ -1,7 +1,7 @@
 (require 'day21)
 (require 'buttercup)
 
-(describe "Day 21"
+(describe "--- Day 21: Monkey Math ---"
   (describe "part 1"
     (it "replicates the example"
       (expect (day21/part-1 (advent/read-problem-lines 21 :example))
@@ -9,10 +9,10 @@
     (it "solves the problem"
       (expect (day21/part-1 (advent/read-problem-lines 21 :problem))
               :to-be 291425799367130)))
-  (xdescribe "part 2"
+  (describe "part 2"
     (it "replicates the example"
       (expect (day21/part-2 (advent/read-problem-lines 21 :example))
-              :to-be 42))
-    (xit "solves the problem"
+              :to-be 301))
+    (it "solves the problem"
       (expect (day21/part-2 (advent/read-problem-lines 21 :problem))
-              :to-be 42))))
+              :to-be 3219579395609))))
--- day21.el
+++ day21.el
@@ -5,7 +5,8 @@
 (defconst example (advent/read-problem-lines 21 :example))
 (defconst problem (advent/read-problem-lines 21 :problem))
 
-(defconst day21/root-monkey :root)
+(defconst day21/root-monkey "root")
+(defconst day21/me "humn")
 
 (defun day21/strip-colon (token)
   (s-replace ":" "" token))
@@ -44,6 +45,9 @@
     (--map (day21/read-monkey table it) lines)
     table))
 
+(setq e (day21/read-problem example))
+(setq p (day21/read-problem problem))
+
 (defun day21/solve-expression! (monkeys value expression)
   (seq-let (operation op1 op2) expression
     (let ((result (funcall operation
@@ -59,13 +63,105 @@
       (day21/solve-expression! monkeys value raw-value))))
 
 (defun day21/get-root-value! (monkeys)
-  (day21/compute-value! monkeys "root"))
+  (day21/compute-value! monkeys day21/root-monkey))
 
 (defun day21/part-1 (lines)
   (setq max-specpdl-size 10000) ;TODO/FIXME why do I need this only for the console?
   (day21/get-root-value! (day21/read-problem lines)))
 
+(defun day21/get-root-subordinates (monkeys)
+  (seq-let (_ sub1 sub2) (advent/get monkeys day21/root-monkey)
+    (list sub1 sub2)))
+
+;; too simple to be true
+(defun day21/dumb-compute-subordinates (monkeys input)
+  (let ((copy (copy-hash-table monkeys)))
+    (advent/put copy day21/me input)
+    (--map (day21/compute-value! copy it)
+           (day21/get-root-subordinates monkeys))))
+
+(defun day21/try-resolve-number! (monkeys name-or-number)
+  (unless (numberp name-or-number)
+    (let ((result (advent/get monkeys name-or-number)))
+      (and (numberp result) result))))
+
+(defun day21/simplify (monkeys)
+  (let ((copy (copy-hash-table monkeys))
+        (optimizing -1))
+    (remhash day21/me copy)
+    (while (not (zerop optimizing))
+      (setq optimizing 0)
+      (advent/-each-hash copy
+        (unless (numberp it-value)
+          (seq-let (op v1 v2) it-value
+            (if-let ((new-v1 (day21/try-resolve-number! copy v1)))
+                (progn
+                  (setq optimizing (1+ optimizing))
+                  (setq v1 new-v1)))
+            (if-let ((new-v2 (day21/try-resolve-number! copy v2)))
+                (progn
+                  (setq optimizing (1+ optimizing))
+                  (setq v2 new-v2)))
+            (if (and (numberp v1) (numberp v2))
+                (advent/put copy it-key (funcall op v1 v2))
+              (advent/put copy it-key (list op v1 v2)))))))
+    copy))
+
+(defun day21/resolve-simbolic-expression (monkeys value)
+  (if (numberp value)
+      value
+    (let ((content (advent/get monkeys value :me)))
+      (cond
+       ((eq content :me) day21/me)
+       ((numberp content) content)
+       (t (list (elt content 0)
+                (day21/resolve-simbolic-expression monkeys (elt content 1))
+                (day21/resolve-simbolic-expression monkeys (elt content 2))))))))
+
+(defun day21/get-equation (monkeys)
+    (let ((simplified (day21/simplify monkeys)))
+      (seq-let (expr1 expr2) (--map (day21/resolve-simbolic-expression simplified it)
+                                    (day21/get-root-subordinates simplified))
+        (if (numberp expr1)
+            (list expr2 expr1)
+          (list expr1 expr2)))))
+
+(defun day21/simplify-expr (expr number)
+  (seq-let (op v1 v2) expr
+    (cond
+     ;; x = (/ expr 4) -> expr = 4 * x
+     ((eq op #'/) (list v1 (* number v2)))
+     ((eq op #'-) (if (numberp v1)
+                      ;; x = (- v1 expr) -> expr = - x + v1
+                      (list v2 (- v1 number))
+                    ;; x = (- expr v2) -> expr = x - v2
+                    (list v1 (+ number v2))))
+     ((eq op #'*) (if (numberp v1)
+                      ;; x = (* v1 expr) -> expr = x / v1
+                      (list v2 (/ number v1))
+                    ;; x = (* expr v2) -> expr = x / v2
+                    (list v1 (/ number v2))))
+     ((eq op #'+) (if (numberp v1)
+                      ;; x = (+ v1 expr) -> epr = x - v1
+                      (list v2 (- number v1))
+                    ;; x = (+ expr v2) -> epr = x - v2
+                    (list v1 (- number v2))))
+     (t (error (format "Unexpected condition '%s'" expr))))))
+
+(defun day21/solve-for-human (monkeys)
+  (seq-let (expr number) (day21/get-equation monkeys)
+    (while (not (equal expr day21/me))
+      ;; TODO/FIXME just use the list
+      (seq-let (new-expr new-number) (day21/simplify-expr expr number)
+        (setq expr new-expr)
+        (setq number new-number)))
+    number))
+
+(defun day21/debug-print-monkeys (monkeys)
+  (advent/-each-hash monkeys
+    (print (format "%s: %s" it-key it-value))))
+
 (defun day21/part-2 (lines)
-  (error "Not yet implemented"))
+  (day21/solve-for-human (day21/read-problem lines)))
 
 (provide 'day21)
