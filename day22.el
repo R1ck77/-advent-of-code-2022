b09a1a777fdbbb1ee9b260832f5e064e692e11b9
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2022-12-21 12:02:27 +0100

    Add boilerplate files for day 22

---
+++ day22.el
@@ -0,0 +1,11 @@
+(require 'dash)
+(require 'advent-utils)
+(require 's)
+
+(defun day22/part-1 (lines)
+  (error "Not yet implemented"))
+
+(defun day22/part-2 (lines)
+  (error "Not yet implemented"))
+
+(provide 'day22)
