dff7eac2947eb59c6c996badfa47a77a981d6eb5
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2022-12-22 17:56:52 +0100

    Bugged solution

--- day22-test.el
+++ day22-test.el
@@ -9,10 +9,10 @@
     (it "solves the problem"
       (expect (day22/part-1 (advent/read-blocks-of-lines 22 :problem))
               :to-be 47462)))
-  (xdescribe "part 2"
+  (describe "part 2"
     (it "replicates the example"
       (expect (day22/part-2 (advent/read-blocks-of-lines 22 :example))
-              :to-be 42))
+              :to-be 5031))
     (xit "solves the problem"
       (expect (day22/part-2 (advent/read-blocks-of-lines 22 :problem))
               :to-be 42))))
--- day22.el
+++ day22.el
@@ -9,6 +9,11 @@
 (defconst day22/floor 1)
 (defconst day22/wall 2)
 
+(defconst day22/right 0)
+(defconst day22/down 1)
+(defconst day22/left 2)
+(defconst day22/up 3)
+
 (defun day22/take-next-movement (line)
   (let ((movement-size (length
                         (--take-while (and (>= it ?0)
@@ -101,13 +106,6 @@
                       :direction (day22/rotate (day22-state-direction state)
                                                (cadar moves))
                       :moves (cdr moves))))
-
-(defun day22/direction-to-displacement (direction)
-  (case direction
-    (0 '(1 . 0))
-    (1 '(0 . -1))
-    (2 '(-1 . 0))
-    (3 '(0 . 1))))
 
 (defun day22/get-tile (board position)
   (advent/grid-get board position))
@@ -206,19 +204,6 @@
     (3 (day22/next-up-coordinate board position))
     (t (error "Unexpected direction"))))
 
-(defvar *day22/displacements-cache* (advent/table))
-
-;; TODO/FIXME Not yet used
-(defun day22/get-next-tile-coordinate-cached (board position direction)
-  (if-let ((cached-next-tile-coordinate (advent/get *day22/displacements-cache* (list position
-                                                                                      direction))))
-      cached-next-tile-coordinate
-    (let ((next-tile-coordinate (day22/get-next-tile-coordinate board position direction)))
-      (advent/put *day22/displacements-cache*
-                  (list position direction)
-                  next-tile-coordinate)
-      next-tile-coordinate)))
-
 (defun day22/new-position (board position direction)
   "Returns the new position or nil in case of wall"
   (let* ((move (day22/get-next-tile-coordinate board position direction))
@@ -292,7 +277,6 @@
     ))
 
 (defun day22/consume-moves (state)
-  (setq *day22/displacements-cache* (advent/table))
   ;(day22/debug-print-state state)
   (while (day22-state-moves state)
     (setq state (day22/move state))
@@ -308,9 +292,203 @@
        direction)))
 
 (defun day22/part-1 (line-blocks)
-  (day22/compute-password (day22/consume-moves (day22/create-initial-state (day22/read-problem line-blocks)))))
+  (day22/compute-password
+   (day22/consume-moves
+    (day22/create-initial-state
+     (day22/read-problem line-blocks)))))
 
-(defun day22/part-2 (lines)
-  (error "Not yet implemented"))
+(defun day22/on-face (side pos corner)
+  (let ((row (car pos))
+        (column (cdr pos))
+        (min-row (car corner))
+        (min-column (cdr corner)))
+    (and (>= row min-row) (>= column min-column)
+         (< row (+ side min-row)) (< column (+ side min-column)))))
+
+(defun day22/get-face-start-corner (M index)
+  (elt (list
+        nil
+        (cons 0 (* 2 M))
+        (cons M 0)
+        (cons M M) 
+        (cons M (* 2 M)) 
+        (cons (* 2 M) (* 2 M)) 
+        (cons (* 2 M) (* 3 M)) )
+       index))
+
+(defun day22/get-face (M pos)
+  (let ((face (cond
+               ((day22/on-face M pos (cons 0 (* 2 M))) 1)
+               ((day22/on-face M pos (cons M 0)) 2)
+               ((day22/on-face M pos (cons M M)) 3)
+               ((day22/on-face M pos (cons M (* 2 M))) 4)
+               ((day22/on-face M pos (cons (* 2 M) (* 2 M))) 5)
+               ((day22/on-face M pos (cons (* 2 M) (* 3 M))) 6)
+               (t nil))))
+    (advent/assert face)
+    face))
+
+(defun day22/is-on-map? (M pos)
+  (and (>= (car pos) 0)
+       (>= (cdr pos) 0)
+       (< (car pos) (* 3 M))
+       (< (cdr pos) (* 4 M))))
+
+;; The last form should be fed the local coordinates for the starting face modulo M
+(defvar day22/topology `(nil            ;we start from 1
+                         ;; 1
+                         (:right (6 day22/left (list (- M row 1) (1- M)))
+                          :down (4 day22/down (list row 0))
+                          :left (3 day22/down (list 0 row))
+                          :up (2 day22/down (list 0 (- M column 1))))
+                         ;; 2
+                         (:right (3 day22/right (list row 0))
+                          :down (5 day22/up (list (- M row 1)  column))
+                          :left (6 day22/up (list (1- M) (- M row 1)))
+                          :up (1 day22/down (list 0 (- M column 1))))
+                         ;; 3
+                         (:right (4 day22/right (list row 0))
+                          :down (5 day22/right (list (- M column 1) 0))
+                          :left (2 day22/left (list row (1- M)))
+                          :up (1 day22/right (list column 0)))
+                         ;; 4
+                         (:right (6 day22/down (list 0 (- M row 1)))
+                          :down (5 day22/down (list 0 column))
+                          :left (3 day22/left (list row (1- M)))
+                          :up (1 day22/up (list (1- M) column)))
+                         ;; 5
+                         (:right (6 day22/right (list row 0))
+                          :down (2 day22/up (list (1- M) (- M column 1)))
+                          :left (3 day22/up (list (1- M) (- M row 1)))
+                          :up (4 day22/up (list (1- M) column)))
+                         ;; 6
+                         (:right (1 day22/left (list (- M row 1) (1- M)))
+                          :down (2 day22/right (list (- M column 1) 0))
+                          :left (5 day22/left (list row (1- M)))
+                          :up (4 day22/left (list (- M column 1) (1- M))))))
+
+(defun day22/direction-to-displacement (direction)
+  (case direction
+    (0 '(1 . 0))
+    (1 '(0 . -1))
+    (2 '(-1 . 0))
+    (3 '(0 . 1))))
+
+
+(defun day22/pos-to-local (M face pos)
+  (advent/assert face)
+  (let ((start-corner (day22/get-face-start-corner M face)))
+    (cons (- (car pos) (car start-corner))
+          (- (cdr pos) (cdr start-corner)))))
+
+(defun day22/local-to-pos (M face local)
+  (advent/assert face)
+  (let ((start-corner (day22/get-face-start-corner M face)))
+    (cons (+ (car local) (car start-corner))
+          (+ (cdr local) (cdr start-corner)))))
+
+(defun day22/candidate-coordinate (pos direction)
+  (let ((displacement (day22/direction-to-displacement direction)))    
+    (cons (+ (car pos) (car displacement))
+          (+ (cdr pos) (cdr displacement)))))
+
+;; TODO/FIXME this is so stupid…
+(defun day22/find-wrap-direction (direction)
+  (case direction
+    (0 :right)
+    (1 :down)
+    (2 :left)
+    (3 :up)))
+
+(defun day22/compute-new-orientation (M old-face old-wrapped-local overflow-direction)
+  (seq-let (new-face new-direction coord-sexpr)
+      (plist-get (elt day22/topology old-face) overflow-direction)
+    (let ((new-local-coordinates (eval coord-sexpr
+                                       (list (cons 'M M)
+                                             (cons 'row (car old-wrapped-local))
+                                             (cons 'column (cdr old-wrapped-local))))))
+      (list new-face new-direction (cons (car new-local-coordinates)
+                                         (cadr new-local-coordinates))))))
+
+;;; M is rows /3
+(defun day22/next-pos-dir (M pos-dir)
+  (let* ((current-pos (car pos-dir))
+        (current-dir (cadr pos-dir))
+        (current-face (day22/get-face M current-pos))
+        (current-local (day22/pos-to-local M current-face current-pos)))
+    (let* ((next-local (day22/candidate-coordinate current-local current-dir))
+           (wrapped-local (cons (mod (car next-local) M)
+                                (mod (cdr next-local) M))))
+      (if (equal next-local wrapped-local)
+          ;; Same face, same-direction: just return the next coordinates converted to global
+          (list (day22/local-to-pos M current-face next-local) current-dir)
+        ;; Different face somehow: find where I'm overflowing and use that to compute
+        ;; the new face, direction and local coordinate
+        (seq-let (new-face new-direction new-local-coordinates)
+            (day22/compute-new-orientation M current-face wrapped-local (day22/find-wrap-direction current-dir))
+          (list (day22/local-to-pos M new-face new-local-coordinates)
+                new-direction))))))
+
+
+;; TODO/FIXME this could be extracted, but whatever
+(defun day22/new-3d-position-direction (board position direction)
+  "Returns the new position or nil in case of wall"
+  (let* ((M (/ (length board) 3))
+         (new-pos-direction (day22/next-pos-dir M (list position direction)))
+         (tile (day22/get-tile board (car new-pos-direction))))
+    (cond 
+     ((= day22/empty tile)
+      (error "Unexpected empty tile"))
+     ((= day22/floor tile)
+      new-pos-direction)
+     ((= day22/wall tile)
+      nil))))
+
+(defun day22/translate-3d (board position direction translation)
+  (let ((steps 0)
+        (stop))
+    (while (and (not stop) (< steps translation )) 
+      (if-let ((new-position-direction (day22/new-3d-position-direction board
+                                                                        position
+                                                                        direction)))
+          (progn
+            (setq position (car new-position-direction))
+            (setq direction (cadr new-position-direction)))        
+        (setq stop t))
+      (setq steps (1+ steps)))
+    (list position direction)))
+
+(defun day22/translate-3d-state (state)
+  (let ((board (day22-state-board state))
+        (direction (day22-state-direction state))
+        (moves (day22-state-moves state)))
+    (let ((new-pos-dir (day22/translate-3d board
+                                           (day22-state-pos state)
+                                           direction
+                                           (caar moves))))      
+      (make-day22-state :board board
+                        :pos (car new-pos-dir)
+                        :direction (cadr new-pos-dir)
+                        :moves (cdr moves)))))
+
+(defun day22/move-3d (state)
+  (let ((moves (day22-state-moves state)))
+    (if (car (car moves))
+        (day22/translate-3d-state state)
+      (day22/rotate-state state))))
+
+(defun day22/consume-moves-3d (state)
+  ;(day22/debug-print-state state)
+  (while (day22-state-moves state)
+    (setq state (day22/move-3d state))
+    ;(day22/debug-print-state state)
+    )
+  state)
+
+(defun day22/part-2 (line-blocks)
+  (day22/compute-password
+   (day22/consume-moves-3d
+    (day22/create-initial-state
+     (day22/read-problem line-blocks)))))
 
 (provide 'day22)
