8b41ee6bd6fe6f7330dd054486867f4b493265df
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2022-12-22 09:12:47 +0100

    Another failure

--- day19.el
+++ day19.el
@@ -18,7 +18,7 @@
                               day19/geo-index))
 
 (defstruct day19-bprint "Blueprint description"
-  id costs)
+  id costs ideal-times)
 
 (defun day19/read-geo-r (definition)
   (seq-let (_ s-ore s-obs)
@@ -69,13 +69,50 @@
                        :costs (list (day19/read-ore-r raw-ore-r)
                                     (day19/read-clay-r raw-clay-r)
                                     (day19/read-obs-r raw-obs-r)
-                                    (day19/read-geo-r raw-geo-r)))))
+                                    (day19/read-geo-r raw-geo-r))
+                       :ideal-times nil)))
 
 (defun day19/op (f v1 v2)
   (--map (funcall f (car it) (cdr it)) (-zip v1 v2)))
 
-(defun day19/vop (f v m)
-  (--map (funcall f it m) v))
+(defun day19/minimum-turns-before-construction (cost)
+  "Time required to have resources >= res starting without robots.
+
+It assumes - ideally - that all the resources to build infinite robots are present"
+  (ceiling (/ (1- (sqrt (+ 1 (* 8 cost)))) 2)))
+
+(defun day19/turns-for-enough-clay-for-obs (bprint)
+  "Irrealistic number of turns required to have enough clay to build an obsidian robot
+
+Assuming no initial clay robot is present"
+  ;; TODO/FIXME not sure about a missing +1
+  (day19/minimum-turns-before-construction (elt (elt (day19-bprint-costs bprint)
+                                                     day19/obs-index)
+                                                day19/clay-index)))
+
+(defun day19/turns-for-enough-obs-for-geo (bprint)
+  "Irrealistic number of turns required to have enough obsidian to build a geo robot
+
+Assuming no initial obsidian robot is present"
+  ;; TODO/FIXME not sure about a missing +1
+  (day19/minimum-turns-before-construction (elt (elt (day19-bprint-costs bprint)
+                                                     day19/geo-index)
+                                                day19/obs-index)))
+
+(defun day19/turns-for-enough-clay-for-geo (bprint)
+  "Irrealistic number of turns require to have enough clay to build a geo robot
+
+Assums no clay robot is present and that perfect production of clay robots and obsidian occurs"
+  ;; TODO/FIXME not sure about a missing +1
+  (+ (day19/turns-for-enough-clay-for-obs bprint)
+     (day19/turns-for-enough-obs-for-geo bprint)))
+
+(defun day19/add-ideal-times-to-blueprint! (bprint)
+  (setf (day19-bprint-ideal-times bprint) (cons (day19/turns-for-enough-clay-for-geo bprint)
+                                                (day19/turns-for-enough-obs-for-geo bprint)))
+  bprint)
+
+
 
 (defstruct day19-state "Simulation state"
            resources
@@ -87,33 +124,8 @@
                     :resources '(0 0 0 0)
                     :robots '(1 0 0 0)))
 
-(defun day19/is-buildable? (resources cost)
-  (> (apply #'min (day19/op (lambda (a b)
-                              (if (zerop b)
-                                  most-positive-fixnum
-                                (/ a b)))
-                            resources cost))
-     0))
-
-(defun day19/sub-mul (v1 v2 m)
-  "res = v1 - m * v2"
-  (day19/op (lambda (a b) (- a (* m b))) v1 v2))
-
-(defun day19/build-cost (costs build)
-  (--reduce (day19/op #'+ acc it)
-            (list (day19/vop #'* (elt costs 0) (elt build 0))
-                  (day19/vop #'* (elt costs 1) (elt build 1))
-                  (day19/vop #'* (elt costs 2) (elt build 2))
-                  (day19/vop #'* (elt costs 3) (elt build 3)))))
-
 (defun day19/compute-new-resources (current gain costs)
   (day19/op #'- (day19/op #'+ current gain) costs))
-
-(defun day19/get-buildable-robots (bprint state)
-  "Returns a list of robots that can be built"
-  (let ((resources (day19-state-resources state))
-        (costs (day19-bprint-costs bprint)))
-    (--map (day19/is-buildable? resources (elt costs it)) (number-sequence 0 3))))
 
 (defun day19/evolve-resources (now current robots)
   "Evolve the resources with the status quo until day 24"
@@ -163,14 +175,49 @@
 
 (defun day19/state-score (state)
   (when state
-    (elt (day19-state-resources state) day19/geo-index)))
+    (elt (day19-state-resources (day19/evolve-state-resources-to-end state)) day19/geo-index)))
 
-;; TODO/FIXME not sure...
+(defun day19/compute-maximal (bprint state)
+  (or 1000
+   (let ((costs (day19-bprint-costs bprint))
+         (robots (day19-state-robots state))
+         (end-resources (day19-state-resources (day19/evolve-state-resources-to-end state))))
+     ;; easy version: don't count how many resources I have, only how many I built
+     ;; also don't check anything but geode robots
+     )))
+
+(defun day19/compute-production-delay (bprint state)
+    (let ((robots (day19-state-robots state))
+          (now (day19-state-time state)))
+      (cond
+       ((zerop (elt robots day19/clay-index))
+        (car (day19-bprint-ideal-times bprint)))
+       ((zerop (elt robots day19/obs-index))
+        (cdr (day19-bprint-ideal-times bprint)))
+       ((zerop (elt robots day19/geo-index))
+        0) ;TODO/FIXME underestimated. Probably it's 1
+       (t 0))))
+
+(defun day19/maximum-ideal-result (state production-delay)
+  (let ((actual-production (elt (day19-state-resources (day19/evolve-state-resources-to-end state)) day19/geo-index))
+        (available-time (- day19/total-time (day19-state-time state) production-delay)))
+    (+ actual-production (/ (* available-time (1- available-time)) 2))))
+
+(defun day19/ideal-result (bprint state)
+  (let ((delay (day19/compute-production-delay bprint state))
+        (now (day19-state-time state)))
+    (let ((result (if (> (+ delay now) day19/total-time)
+               (elt (day19-state-resources (day19/evolve-state-resources-to-end state)) day19/geo-index)
+               (day19/maximum-ideal-result state delay))
+                  ))
+      (if (zerop result) -1 0))))
+
 (defun day19/evaluate-sequence-with-check (bprint state path last-state)
-  
-  (let ((i))
-    
-    (day19/jump-state-to-construction bprint last-state (car path))))
+  ;; TODO/FIXME is there a teoretical max? Yep
+  (if (and (< (--count (= it day19/ore-index) path) 8)
+           (> (day19/compute-maximal bprint last-state) *best-result*))    
+      (day19/jump-state-to-construction bprint last-state (car path))
+    nil))
 
 (defun day19/next-combinations (bprint state path last-state)
   (--map (list (day19/state-score (cadr it))
@@ -182,9 +229,11 @@
                                                                     state
                                                                     it
                                                                     last-state))
-                          (--map (cons it path) day19/indices)))))
+                          (--map (cons it path) (reverse day19/indices))))))
 
+;;; TODO/FIXME I definitely have to remove those horrors :p
 (defvar *best-result* )
+
 (defun day19/search-best (bprint state path last-state)
   (comment
     (print (reverse path))
@@ -195,11 +244,9 @@
         (apply #'max (or (--map (day19/search-best bprint state (elt it 2) (elt it 1))
                                 score-state-path-list)
                          '(-1)))
-      (let ((result (day19/state-score (if path
-                                    (day19/evaluate-sequence bprint state (reverse path))
-                                    state))))
+      (let ((result (day19/state-score last-state)))
         (when (> result *best-result*)
-          (print result)
+          (print (format "%s -> %s" (reverse path) result))
           (setq *best-result* result)
           (sit-for 0.01))
         result))))
@@ -215,7 +262,7 @@
   (day19/state-score (car (nreverse (day19/evolve-blueprint bprint)))))
 
 (defun day19/read-problem (lines)
-  (-map #'day19/read-blueprint lines))
+  (-map #'day19/add-ideal-times-to-blueprint! (-map #'day19/read-blueprint lines)))
 
 (setq e (day19/read-problem example))
 (setq p (day19/read-problem problem))
