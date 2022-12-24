8d7674fb7b03dda51cef9412cd626752cdfe5d57
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2022-12-24 12:33:39 +0100

    Add data for day 24

--- day24-test.el
+++ day24-test.el
@@ -1,11 +1,11 @@
 (require 'day24)
 (require 'buttercup)
 
-(describe "Day 24"
+(describe "--- Day 24: Blizzard Basin ---"
   (describe "part 1"
     (it "replicates the example"
       (expect (day24/part-1 (advent/read-problem-lines 24 :example))
-              :to-be 42))
+              :to-be 18))
     (xit "solves the problem"
       (expect (day24/part-1 (advent/read-problem-lines 24 :problem))
               :to-be 42)))
--- day24.el
+++ day24.el
@@ -2,6 +2,15 @@
 (require 'advent-utils)
 (require 's)
 
+(defconst example (advent/read-problem-lines 24 :example))
+(defconst problem (advent/read-problem-lines 24 :problem))
+
+(defun day24/read-problem (lines)
+  )
+
+;;(setq e (day24/read-problem example))
+;;(setq p (day24/read-problem example))
+
 (defun day24/part-1 (lines)
   (error "Not yet implemented"))
 
