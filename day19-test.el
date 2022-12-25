6d79be7e34bdb27eae2bac7b7e9f9f389e8f92a8
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2022-12-25 05:34:21 +0100

    Day 19 part 2 solved
    yay.

--- day19-test.el
+++ day19-test.el
@@ -2,17 +2,17 @@
 (require 'buttercup)
 
 (describe "--- Day 19: Not Enough Minerals ---"
-  (describe "part 1"
+  (xdescribe "part 1"
     (it "replicates the example"
       (expect (day19/part-1 (advent/read-problem-lines 19 :example))
               :to-be 33))
     (it "solves the problem"
       (expect (day19/part-1 (advent/read-problem-lines 19 :problem))
               :to-be 1653)))
-  (xdescribe "part 2"
+  (describe "part 2"
     (it "replicates the example"
       (expect (day19/part-2 (advent/read-problem-lines 19 :example))
-              :to-be 42))
+              :to-be 3472))
     (it "solves the problem"
       (expect (day19/part-2 (advent/read-problem-lines 19 :problem))
-              :to-be 42))))
+              :to-be 4212))))
